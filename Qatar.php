6d221d65b4546b965a988166836de7bdaffeef82
ayalaZ <zenon2gm1994@gmail.com>
2022-04-01 16:39:09 -0600

    Iniciando repositorio

---
+++ Qatar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mundial</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/Qatar.css">
+</head>
+<body>
+   <div class="bg-default text-white rounded" id="encabezado">
+       <h1 class="display-4">Mi mundial Qatar 2022</h1>
+   </div>
+</body>
+</html>
